e673ae30df11e8e5b8c4a48ce0c492b5c7ffdccd
PromptBros <prompt.bros@gmail.com>
2023-04-06 13:58:37 +0100

    Added image gallery

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
 const BASE_URL = "https://promptbros.github.io/CDN/ChatGPT/";
 
-function fetchAndDisplayTextFile(filename, fileItem) {
-  fetch(BASE_URL + filename)
-    .then((response) => response.text())
-    .then((text) => {
-      const textareaContainer = document.createElement("div");
-      textareaContainer.classList.add("flex");
-      textareaContainer.innerHTML = `
-        <textarea class="w-full h-32 p-2 border mb-3" readonly>${text}</textarea>
-        <div class="ml-2 flex items-start">
-          <button class="bg-green-600 text-white px-3 py-1 rounded" data-clipboard-text="${text}">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
-            </svg>
-          </button>
-        </div>
-      `;
-      fileItem.appendChild(textareaContainer);
-    })
-    .catch((error) => {
-      console.error("Error fetching text file:", error);
-    });
+async function fetchAndDisplayTextFile(filename, fileItem) {
+  try {
+    const response = await fetch(BASE_URL + filename);
+    const text = await response.text();
+
+    const textareaContainer = document.createElement("div");
+    textareaContainer.classList.add("flex");
+    textareaContainer.innerHTML = `
+      <textarea class="w-full h-32 p-2 border mb-3" readonly>${text}</textarea>
+      <div class="ml-2 flex items-start">
+        <button class="bg-green-600 text-white px-3 py-1 rounded" data-clipboard-text="${text}">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+          </svg>
+        </button>
+      </div>
+    `;
+    fileItem.appendChild(textareaContainer);
+  } catch (error) {
+    console.error("Error fetching text file:", error);
+  }
 }
 
-function displayFiles(files) {
+async function fetchAndDisplayMarkdownFile(fileName, fileItem) {
+  try {
+    const response = await fetch(BASE_URL + fileName);
+    const markdown = await response.text();
+    const html = marked.parse(markdown);
+    const instructionsContainer = document.createElement("div");
+    instructionsContainer.classList.add("markdown", "mt-3");
+    instructionsContainer.innerHTML = html;
+    fileItem.appendChild(instructionsContainer);
+  } catch (error) {
+    console.error("Error fetching Markdown file:", error);
+  }
+}
+
+async function displayFiles(files) {
   const fileList = document.getElementById("file-list");
 
   fileList.innerHTML = "";
-  files.forEach((file) => {
-
+  for (const file of files) {
     const fileItem = document.createElement("div");
     fileItem.classList.add("bg-white", "p-4", "rounded", "shadow", "border", "mb-6");
     fileList.appendChild(fileItem);
@@ -57,50 +70,49 @@ function displayFiles(files) {
     description.textContent = file.content.description;
     contentContainer.appendChild(description);
 
-    fetchAndDisplayTextFile(file.fileName, contentContainer);
+    await fetchAndDisplayTextFile(file.fileName, contentContainer);
     if (file.fileName) {
       const instructionsFilename = file.fileName.replace(".txt", ".md");
-      fetchAndDisplayMarkdownFile(instructionsFilename, fileItem);
+      await fetchAndDisplayMarkdownFile(instructionsFilename, fileItem);
     }
-  });
+
+    if (file.content.images) {
+      const galleryContainer = document.createElement("div");
+      galleryContainer.classList.add("grid", "grid-cols-3", "gap-4", "mb-6");
+      fileItem.appendChild(galleryContainer);
+
+      for (const image of file.content.images) {
+        const imgElement = document.createElement("img");
+        imgElement.src = BASE_URL + image;
+        imgElement.classList.add("w-full", "h-auto", "rounded");
+        galleryContainer.appendChild(imgElement);
+      }
+    }
+  }
 
   new ClipboardJS("[data-clipboard-text]");
 }
 
-
-function fetchAndDisplayJSON(url) {
-  fetch(url)
-      .then((response) => response.json())
-      .then((files) => displayFiles(files))
-      .catch((error) => {
-          console.error("Error fetching JSON:", error);
-      });
+async function fetchAndDisplayJSON(url) {
+  try {
+    const response = await fetch(url);
+    const files = await response.json();
+    displayFiles(files);
+  } catch (error) {
+    console.error("Error fetching JSON:", error);
+  }
 }
 
-function fetchAndDisplayMarkdownFile(fileName, fileItem) {
-  fetch(BASE_URL + fileName)
-    .then((response) => response.text())
-    .then((markdown) => {
-      const html = marked.parse(markdown);
-      const instructionsContainer = document.createElement("div");
-      instructionsContainer.classList.add("markdown", "mt-3");
-      instructionsContainer.innerHTML = html;
-      fileItem.appendChild(instructionsContainer);
-    })
-    .catch((error) => {
-      console.error("Error fetching Markdown file:", error);
-    });
-}
-
-document.getElementById("url-input").addEventListener("input", (event) => {
+document.getElementById("url-input").addEventListener("input", async (event) => {
   const url = event.target.value;
 
   if (!url) {
-      return;
+    return;
   }
 
-  fetchAndDisplayJSON(url);
+  await fetchAndDisplayJSON(url);
 });
 
 // Load the default template.json file from the provided URL
 fetchAndDisplayJSON(BASE_URL + "prompt.json");
+
