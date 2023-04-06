a8cd87ccdcaa99136b9df145907becae03baa7ad
PromptBros <prompt.bros@gmail.com>
2023-04-06 12:57:59 +0100

    Style changes and improvements to the CVO manual

--- app.js
+++ app.js
@@ -9,11 +9,9 @@ function fetchAndDisplayTextFile(filename, fileItem) {
       textareaContainer.innerHTML = `
         <textarea class="w-full h-32 p-2 border mb-3" readonly>${text}</textarea>
         <div class="ml-2 flex items-start">
-          <button class="bg-cyan-600 text-white px-3 py-1 rounded" data-clipboard-text="${text}">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-              <path fill-rule="evenodd" d="M17.663 3.118c.225.015.45.032.673.05C19.876 3.298 21 4.604 21 6.109v9.642a3 3 0 01-3 3V16.5c0-5.922-4.576-10.775-10.384-11.217.324-1.132 1.3-2.01 2.548-2.114.224-.019.448-.036.673-.051A3 3 0 0113.5 1.5H15a3 3 0 012.663 1.618zM12 4.5A1.5 1.5 0 0113.5 3H15a1.5 1.5 0 011.5 1.5H12z" clip-rule="evenodd" />
-              <path d="M3 8.625c0-1.036.84-1.875 1.875-1.875h.375A3.75 3.75 0 019 10.5v1.875c0 1.036.84 1.875 1.875 1.875h1.875A3.75 3.75 0 0116.5 18v2.625c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625v-12z" />
-              <path d="M10.5 10.5a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963 5.23 5.23 0 00-3.434-1.279h-1.875a.375.375 0 01-.375-.375V10.5z" />
+          <button class="bg-green-600 text-white px-3 py-1 rounded" data-clipboard-text="${text}">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
             </svg>
           </button>
         </div>
@@ -30,31 +28,36 @@ function displayFiles(files) {
 
   fileList.innerHTML = "";
   files.forEach((file) => {
+
     const fileItem = document.createElement("div");
-    fileItem.classList.add("bg-slate-200", "p-4", "rounded", "shadow", "border", "mb-6");
+    fileItem.classList.add("bg-white", "p-4", "rounded", "shadow", "border", "mb-6");
     fileList.appendChild(fileItem);
+
+    const contentContainer = document.createElement("div");
+    contentContainer.classList.add("bg-neutral-200", "p-4", "rounded", "shadow", "border", "mb-6");
+    fileItem.appendChild(contentContainer);
 
     const title = document.createElement("h3");
     title.classList.add("text-xl", "font-semibold", "mb-3");
     title.textContent = file.content.title;
-    fileItem.appendChild(title);
+    contentContainer.appendChild(title);
 
     const tags = document.createElement("ul");
     tags.classList.add("list-none", "flex", "flex-wrap", "mb-3");
     file.content.tags.forEach((tag) => {
       const tagItem = document.createElement("li");
-      tagItem.classList.add("text-xs", "bg-slate-400", "rounded-full", "text-white", "px-1", "py-1", "mr-2", "mb-2");
+      tagItem.classList.add("text-xs", "bg-gradient-to-tl", "from-pink-500", "to-cyan-400", "text-white", "rounded-full", "px-2", "py-1", "mr-2", "mb-2");
       tagItem.textContent = tag;
       tags.appendChild(tagItem);
     });
-    fileItem.appendChild(tags);
+    contentContainer.appendChild(tags);
 
     const description = document.createElement("p");
     description.classList.add("text-sm", "mb-3");
     description.textContent = file.content.description;
-    fileItem.appendChild(description);
+    contentContainer.appendChild(description);
 
-    fetchAndDisplayTextFile(file.fileName, fileItem);
+    fetchAndDisplayTextFile(file.fileName, contentContainer);
     if (file.fileName) {
       const instructionsFilename = file.fileName.replace(".txt", ".md");
       fetchAndDisplayMarkdownFile(instructionsFilename, fileItem);
@@ -63,6 +66,7 @@ function displayFiles(files) {
 
   new ClipboardJS("[data-clipboard-text]");
 }
+
 
 function fetchAndDisplayJSON(url) {
   fetch(url)
@@ -74,7 +78,6 @@ function fetchAndDisplayJSON(url) {
 }
 
 function fetchAndDisplayMarkdownFile(fileName, fileItem) {
-  console.log(BASE_URL + fileName)
   fetch(BASE_URL + fileName)
     .then((response) => response.text())
     .then((markdown) => {
