e32111866f36332893548ac3fbf1280bbbd51550
PromptBros <prompt.bros@gmail.com>
2023-04-14 18:51:48 +0100

    new pack added

--- assets/js/plp.js
+++ assets/js/plp.js
@@ -13,10 +13,16 @@ function getQueryParam(param) {
 }
 
 function xorDecode(encodedStr, seed) {
+  if (!encodedStr) {
+    console.error("Not a Valid string")
+    return
+  }
+
   const firstChar = encodedStr[0];
   const lastChar = encodedStr[encodedStr.length - 1];
 
-  if (!(firstChar === 'f' && lastChar === '=')) {
+  if (!(firstChar === 'f' && lastChar === 'O')) {
+    console.warn("Not a Valid Prompt ID")
     return
   }
 
@@ -51,33 +57,43 @@ function displayGroups(groups) {
 
   for (const group of groups) {
     const groupContainer = document.createElement("div");
-    groupContainer.classList.add("bg-white", "p-4", "rounded", "shadow", "border");
+    groupContainer.classList.add("group", "relative", "p-6");
     groupList.appendChild(groupContainer);
 
+    const groupHeaderContainer = document.createElement("div");
+    groupHeaderContainer.classList.add("p-6");
+    groupContainer.appendChild(groupHeaderContainer);
+
+    if (group.content.images.length > 0) {
+      const logoContainer = document.createElement("div");
+      logoContainer.classList.add("w-14", "h-14", "overflow-hidden", "flex", "float-left", "mr-4");
+      groupHeaderContainer.appendChild(logoContainer);
+
+      const logoImage = document.createElement("img");
+      logoImage.src = group.content.images[0];
+      logoImage.classList.add("w-full", "h-full");
+      logoContainer.appendChild(logoImage);
+    }
+
     const title = document.createElement("h2");
-    title.classList.add("text-2xl", "font-semibold", "mb-4");
+    title.classList.add("text-2xl", "font-semibold", "mb-2");
     title.textContent = group.content.title;
-    groupContainer.appendChild(title);
+    groupHeaderContainer.appendChild(title);
 
     const description = document.createElement("p");
     description.classList.add("text-sm", "mb-4");
     description.textContent = group.content.description;
-    groupContainer.appendChild(description);
+    groupHeaderContainer.appendChild(description);
 
     const promptList = document.createElement("div");
-    promptList.classList.add("grid", "grid-cols-1", "md:grid-cols-2", "lg:grid-cols-3", "gap-4");
+    promptList.classList.add("grid", "grid-cols-1", "md:grid-cols-3", "lg:grid-cols-4", "gap-3");
     groupContainer.appendChild(promptList);
 
     for (const prompt of group.prompts) {
       const promptCard = document.createElement("a");
       promptCard.href = prompt.path;
-      promptCard.classList.add("bg-gradient-to-tl", "from-pink-500", "to-cyan-400", "p-4", "rounded", "shadow", "text-white", "flex", "flex-col", "items-start");
+      promptCard.classList.add("relative", "h-80", "w-full", "overflow-hidden", "rounded-lg", "bg-white", "group-hover:opacity-75", "shadow-md");
       promptList.appendChild(promptCard);
-
-      const promptText = document.createElement("h3");
-      promptText.classList.add("text-lg", "font-semibold", "mb-2");
-      promptText.textContent = prompt.text;
-      promptCard.appendChild(promptText);
 
       if (prompt.images.length > 0) {
         const imageContainer = document.createElement("div");
@@ -89,6 +105,11 @@ function displayGroups(groups) {
         promptImage.classList.add("w-full", "h-full", "object-cover");
         imageContainer.appendChild(promptImage);
       }
+
+      const promptText = document.createElement("h3");
+      promptText.classList.add("text-lg", "font-semibold", "mb-2");
+      promptText.textContent = prompt.text;
+      promptCard.appendChild(promptText);
     }
   }
 }
