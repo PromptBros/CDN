b334cb4c4602f47efa57e0f974773d485c73c948
PromptBros <prompt.bros@gmail.com>
2023-04-14 15:04:31 +0100

    Several improvements, new pages pdp, plp

--- assets/js/pdp.js
+++ assets/js/pdp.js
@@ -1,13 +1,11 @@
-const BASE_URL = "https://promptbros.github.io/CDN/";
-const seed = "PromtBros 2023";
+const BASE_URL = "https://promptbros.github.io/CDN";
+const SEED = "PromtBros 2023";
 const encodedUrlParam = getQueryParam("data");
 
-const dencodedStr = xorDecode(encodedUrlParam, seed);
-const decodeUrl = BASE_URL + dencodedStr;
-const baseUrl = trimFilenameFromUrl(decodeUrl) ;
-
-console.log(baseUrl);
-
+const decodedStr = xorDecode(encodedUrlParam, SEED);
+const baseJson = BASE_URL + decodedStr;
+let baseUrl = trimFilenameFromUrl(baseJson);
+console.log("dec:", decodedStr)
 function getQueryParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
@@ -15,12 +13,26 @@ function getQueryParam(param) {
 }
 
 function xorDecode(encodedStr, seed) {
-  const str = atob(encodedStr); // Decode the base64 encoded string
-  let result = "";
-  for (let i = 0; i < str.length; i++) {
-    result += String.fromCharCode(str.charCodeAt(i) ^ seed.charCodeAt(i % seed.length));
+  const firstChar = encodedStr[0];
+  const lastChar = encodedStr[encodedStr.length - 1];
+
+  if (!(firstChar === 'f' && lastChar === '=')) {
+    return
   }
-  return result;
+
+  try {
+    const str = atob(encodedStr);
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+      result += String.fromCharCode(str.charCodeAt(i) ^ seed.charCodeAt(i % seed.length));
+    }
+
+    return result;
+  } catch (error) {
+    console.error("Not a valid prompt Id", error);
+    return;
+  }
 }
 
 function trimFilenameFromUrl(url) {
@@ -30,10 +42,12 @@ function trimFilenameFromUrl(url) {
   pathArray.pop();
   const newPath = pathArray.join("/");
   const newUrl = urlObj.origin + newPath + "/";
+
   return newUrl;
 }
 
 async function fetchAndDisplayTextFile(filename, fileItem) {
+  console.log(baseUrl, filename);
   try {
     const response = await fetch(baseUrl + filename);
     const text = await response.text();
@@ -41,9 +55,9 @@ async function fetchAndDisplayTextFile(filename, fileItem) {
     const textareaContainer = document.createElement("div");
     textareaContainer.classList.add("flex");
     textareaContainer.innerHTML = `
-      <textarea class="w-full h-32 p-2 border mb-3" readonly>${text}</textarea>
+      <textarea class="w-full h-64 p-2 border mb-3" readonly>${text}</textarea>
       <div class="ml-2 flex items-start">
-        <button class="bg-green-600 text-white px-3 py-1 rounded" data-clipboard-text="${text}">
+        <button class="bg-green-600 text-white px-3 py-1 rounded hover:bg-green-500" data-clipboard-text="${text}">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
           </svg>
@@ -76,11 +90,11 @@ async function displayFiles(files) {
   fileList.innerHTML = "";
   for (const file of files) {
     const fileItem = document.createElement("div");
-    fileItem.classList.add("bg-white", "p-4", "rounded", "shadow", "border", "mb-6");
+    fileItem.classList.add("mb-6");
     fileList.appendChild(fileItem);
 
     const contentContainer = document.createElement("div");
-    contentContainer.classList.add("bg-neutral-200", "p-4", "rounded", "shadow", "border", "mb-6");
+    contentContainer.classList.add("bg-neutral-100", "p-4", "rounded", "shadow", "border", "mb-6");
     fileItem.appendChild(contentContainer);
 
     const title = document.createElement("h3");
@@ -92,7 +106,7 @@ async function displayFiles(files) {
     tags.classList.add("list-none", "flex", "flex-wrap", "mb-3");
     file.content.tags.forEach((tag) => {
       const tagItem = document.createElement("li");
-      tagItem.classList.add("text-xs", "bg-gradient-to-tl", "from-pink-500", "to-cyan-400", "text-white", "rounded-full", "px-2", "py-1", "mr-2", "mb-2");
+      tagItem.classList.add("text-xs", "bg-slate-400", "text-white", "rounded-full", "px-2", "py-1", "mr-2", "mb-2");
       tagItem.textContent = tag;
       tags.appendChild(tagItem);
     });
@@ -126,9 +140,9 @@ async function displayFiles(files) {
   new ClipboardJS("[data-clipboard-text]");
 }
 
-async function fetchAndDisplayJSON(url) {
+async function fetchAndDisplayJSON(jsonUrl) {
   try {
-    const response = await fetch(url);
+    const response = await fetch(jsonUrl);
     const files = await response.json();
     displayFiles(files);
   } catch (error) {
@@ -137,24 +151,24 @@ async function fetchAndDisplayJSON(url) {
 }
 
 document.getElementById("url-input").addEventListener("input", async (event) => {
-  let encodedUrl = event.target.value;
-  let url = xorDecode(encodedUrl, seed);
+  const encodedUrl = event.target.value;
+  const url = xorDecode(encodedUrl, SEED);
 
   if (!url) {
     return;
   }
 
+  baseUrl = trimFilenameFromUrl(BASE_URL + url);
   fetchAndDisplayJSON(BASE_URL + url);
 
-  console.log(BASE_URL + url);
   const currentUrl = window.location.href;
   const newUrl = currentUrl.split("?")[0] + "?data=" + encodedUrl;
   history.pushState(null, null, newUrl);
 });
 
 // Load the default template.json file from the provided URL
-if (encodedUrlParam) {
-   fetchAndDisplayJSON(decodeUrl);
+if (decodedStr) {
+  fetchAndDisplayJSON(baseJson);
 }
 
 
